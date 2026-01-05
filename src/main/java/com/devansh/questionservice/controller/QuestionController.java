5d3b12ee15a7c9573b1e23f2d9763c036c3ee938
Devansh27112001 <dkansara2711@gmail.com>
2026-01-05 15:29:48 -0500

    Environment variables added

--- src/main/java/com/devansh/questionservice/controller/QuestionController.java
+++ src/main/java/com/devansh/questionservice/controller/QuestionController.java
@@ -6,7 +6,6 @@ import com.devansh.questionservice.model.Question;
 import com.devansh.questionservice.model.QuestionWrapper;
 import com.devansh.questionservice.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
